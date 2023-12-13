8e4ced8ea5bdfcebac1cf1265a7f9b9096116187
DuMark <dumark.allman@yahoo.com>
2023-12-12 21:31:09 -0500

    Phew!

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,25 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About(props) {
+  function checkBio() {
+      if (!props.bio) {
+        return null;      
+      
+    } else if(props.bio === "") {
+        return null
+  } else if(props.bio) {
+    return <p> { props.bio } </p>
+  }
+  }
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2>About Me</h2>   
+      { checkBio() }
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github ={props.github}linkedin ={props.linkedin}/>
     </div>
   );
 }
